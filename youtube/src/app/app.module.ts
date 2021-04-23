dc60a19f0389b36e6abfe804235f8abbe176c8b0
samar jamjom <samarjamjom@gmail.com>
2021-04-23 21:42:59 +0300

    Navigate to the credentials page by clicking on Credentials located on the sidebar menu

--- youtube/src/app/app.module.ts
+++ youtube/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+//ApiKey:AIzaSyCZ5I_wRuRG027fothsJihBacnDIefanQg
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
