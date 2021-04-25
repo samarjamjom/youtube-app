e16aef1b27f657aad7b2627d22bbf33073b6ceab
samar jamjom <samarjamjom@gmail.com>
2021-04-25 14:56:26 +0300

    setup search input component

--- youtube/src/app/search/components/search-input/search-input.component.ts
+++ youtube/src/app/search/components/search-input/search-input.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, ElementRef, Output, EventEmitter } from '@angular/core';
+import { fromEvent } from 'rxjs';
+import { debounceTime, distinctUntilChanged, filter, map, pluck } from 'rxjs/operators'
 
 @Component({
   selector: 'app-search-input',
   templateUrl: './search-input.component.html',
   styleUrls: ['./search-input.component.css']
 })
-export class SearchInputComponent implements OnInit {
+export class SearchInputComponent implements AfterViewInit {
+
+  @ViewChild('input') inputElement: ElementRef;
+  @Output() search = new EventEmitter();
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    fromEvent(this.inputElement.nativeElement, 'keyup')
+      .pipe(
+        debounceTime(500),
+        pluck('target', 'value'),
+        distinctUntilChanged(),
+        filter((val: string) => val.length > 3),
+        map(val => val)
+      )
+      .subscribe(val => {
+        console.log(val)
+        this.search.emit(val);
+      })
   }
 
 }
